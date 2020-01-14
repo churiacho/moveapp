230290fecb781af523884a4a6e3c961965dc2208
Julia Jo <julia.ohyoon@gmail.com>
2020-01-14 13:31:07 -0500

    added heroku url to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,12 +1,12 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: 'https://julia-moveapp.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://moveappjulia.herokuapp.com/' }
     config.action_mailer.delivery_method = :smtp
       ActionMailer::Base.smtp_settings = {
         :user_name => ENV['SENDGRID_USERNAME'],
         :password => ENV['SENDGRID_PASSWORD'],
-        :domain => 'https://julia-moveapp.herokuapp.com/',
+        :domain => 'https://moveappjulia.herokuapp.com/',
         :address => 'smtp.sendgrid.net',
         :port => 587,
         :authentication => :plain,
