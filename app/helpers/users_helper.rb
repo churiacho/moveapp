9c8b03d1d0988cc55af651a4c42f08ea47ca1a77
Julia Jo <julia.ohyoon@gmail.com>
2020-01-15 11:11:08 -0500

    Work on bugs in carrierwave.rb for Heroku push

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,14 @@
+module UsersHelper
+
+    def gravatar_for(user, size: 190)
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        image_tag(gravatar_url, alt: user.first_name + ' ' + user.last_name, class: "gravatar")
+    end
+
+    def header_gravatar_for(user, size: 30)
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        image_tag(gravatar_url, alt: user.first_name + ' ' + user.last_name, class: "rounded-circle header-profile-img")
+    end
+end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,15 @@
 CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
+    config.fog_provider = 'fog/aws' 
     config.fog_credentials = {
-        :provider => 'AWS'
-        :aws_access_key_id => ENV["AWS_ACCESS_KEY_ID"],
-        :aws_secret_access_key => ENV["AWS_SECRET_ACCES_KEY"]
-        :region => ENV['REGION']
-    }
-
-    config.fog_directory = ENV["FOG_DIRECTORY"]
-    config.fog_public = false
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}"}
-
-end
+         provider:               'AWS',
+         aws_access_key_id:      ENV.fetch('AWS_ACCESS_KEY_ID'),
+         aws_secret_access_key:  ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+         region:                 ENV.fetch('REGION')
+         
+   }
+   
+   config.fog_directory = ENV.fetch('FOG_DIRECTORY')
+   config.fog_public     =  false 
+   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+   
+end 
